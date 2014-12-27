e9d99616ecf13b43c87ff652de19fe06bae1a234
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-12-27 11:42:49 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	$('#timmer').countdown('2014/12/29', function(event) {
+		$(this).text(event.strftime('%D días %H:%M:%S'));
+	});
+	$('#btnverbases').click(function(event){
+		verBases();
+	});
+});
+
+/******************* Funcion que permite abrir las bases del concurso */
+function verBases(){
+	var WindowObjectReference = window.open("bases.pdf", "bases_concurso", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
+}
