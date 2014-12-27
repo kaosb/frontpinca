cc5c6ab7878d9f336b6dbca8626c081913fb0f77
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-12-27 20:47:53 -0300

    Cambios

--- index_ini.php
+++ index_ini.php
@@ -11,19 +11,36 @@
 </head>
 <body>
 	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=384241271749937&version=v2.0";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+	<script>
+		window.fbAsyncInit = function() {
+			FB.init({appId: '384241271749937',
+				channelUrl : '//coddea.com/proyectos/fb_cpech_2014/src/channel.php',
+				status     : true, // check login status
+				cookie     : true, // enable cookies to allow the server to access the session
+				xfbml      : true  // parse XFBML
+			});
+			fanGateIni();
+		};
+		(function(d){
+			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement('script'); js.id = id; js.async = true;
+			js.src = "//connect.facebook.net/es_ES/all.js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document));
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=384241271749937&version=v2.0";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
 	</script>
 	<div id="fangate">
 		<div id="btnlike"></div>
 		<div id="btnverbases"></div>
 		<div id="likecanvas">
-			<div class="fb-like" data-href="https://www.facebook.com/preuniversitariocpech" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+			<div class="fb-like" data-href="https://www.facebook.com/preuniversitariocpech" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false" style="position:relative;top:40%;left:35%;"></div>
 			<span id="close">Cerrar</span>
 		</div>
 	</div>
--- js/script.js
+++ js/script.js
@@ -1,14 +1,59 @@
 $(document).ready(function(){
+	/******************* Al cargar el DOM muestra el fangate */
 	$('#fangate').show();
+	/******************* Bind para el boton ver bases que despliega las bases del concurso */
 	$('#btnverbases').click(function(event){
 		verBases();
 	});
+	/******************* Bind para el boton like que despliega el canvas que contiene el boton proporcionado por fb */
 	$('#btnlike').click(function(event){
 		$('#likecanvas').show();
+	});
+	/******************* Bind para el boton cerrar del canvas que contiene el boton like */
+	$('#close').click(function(event){
+		$('#likecanvas').hide();
 	});
 });
 
 /******************* Funcion que permite abrir las bases del concurso */
 function verBases(){
 	var WindowObjectReference = window.open("bases.pdf", "bases_concurso", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
+}
+
+/******************* Revisa que este logeado y chequea los permisos asi tambien los solicita de ser necesario */
+function fanGateIni(){
+	/* isFanCheck("211627285577705"); -> Felipe I. González G. */
+	/* isFanCheck("99723704300"); -> Maternidad Clinica Alemana */
+	FB.getLoginStatus(function(response){
+		if(response && response.authResponse && response.status === 'connected'){
+			isFanCheck("256475461058960");
+		}else{
+			FB.login(function(response){
+				if (response && response.authResponse && response.status === 'connected'){
+					isFanCheck("256475461058960");
+				}else{
+					isFanChecknoPermisions();
+				}
+			},{scope:'user_likes, email, publish_stream'});
+		}
+	});
+}
+
+/******************* Verifica que el usuario sea fan de la pagina indicada. */
+function isFanCheck(page_id){
+	FB.api('/me/likes/'+page_id,function(response) {
+		if(response.data){
+			if(!isEmpty(response.data)){
+				$('#step0').show();
+			}else{
+				$('#fangate').show();
+			}
+		}
+	});
+}
+
+/******************* Que despliega mensaje indicando que es necesario los permisos para usar la aplicacion */
+function isFanChecknoPermisions(){
+	alert("Para usar esta aplicación, es necesario concedas los permisos necesarios para corroborar si ya eres fan de esta pagina.");
+	location.reload();
 }
