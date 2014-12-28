a4315c4e45608e1d9156a54ce96c752fd176f1db
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-12-28 06:39:54 -0500

    index Cleanup

--- index_ini.php
+++ index_ini.php
@@ -20,7 +20,6 @@
 				cookie     : true, // enable cookies to allow the server to access the session
 				xfbml      : true  // parse XFBML
 			});
-			// fanGateIni();
 		};
 		(function(d){
 			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -29,21 +28,10 @@
 			js.src = "//connect.facebook.net/es_ES/all.js";
 			ref.parentNode.insertBefore(js, ref);
 		}(document));
-		// (function(d, s, id) {
-		// 	var js, fjs = d.getElementsByTagName(s)[0];
-		// 	if (d.getElementById(id)) return;
-		// 	js = d.createElement(s); js.id = id;
-		// 	js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=384241271749937&version=v2.0";
-		// 	fjs.parentNode.insertBefore(js, fjs);
-		// }(document, 'script', 'facebook-jssdk'));
 	</script>
 	<div id="fangate">
 		<div id="btnlike"></div>
 		<div class="btnverbases"></div>
-		<!-- <div id="likecanvas">
-			<div class="fb-like" data-href="https://www.facebook.com/preuniversitariocpech" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false" style="position:relative;top:40%;left:35%;"></div>
-			<span id="close">Cerrar</span>
-		</div> -->
 	</div>
 	<div id="step0">
 		<div id="btnjoin"></div>
@@ -54,7 +42,6 @@
 		<form id="imageform" uploadurl="src/guardarImagen.php" posturl="src/guardarParticipante.php" >
 			<input id="imageinput" name="imageinput" type="file" accept="image/*;capture=camera" style="display:none;" capture/>
 		</form>
-		<!-- <a href="#" id="btnenviar">Enviar</a> -->
 	</div>
 	<div id="step2">
 		<input id="score" name="score" type="number" value="0">
