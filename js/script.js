30dd64c35576b0a54bc2f02a90ddd71a5825aee8
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-12-28 06:39:04 -0500

    JS Cleanup

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
 $(document).ready(function(){
 	/******************************************** Objeto Global para almacenar al participante y sus datos */
 	$.participantdata = {};
-	/******************* Al cargar el DOM muestra el fangate */
-	// $('#fangate').show();
 	/******************* Al cargar el DOM muestra el step 0 */
 	$('#step0').show();
 	/******************* Bind para el boton ver bases que despliega las bases del concurso */
 	$('.btnverbases').on('click', function(event){
 		verBases();
 	});
-	/******************* Bind para el boton like que despliega el canvas que contiene el boton proporcionado por fb */
-	// $('#btnlike').on('click', function(event){
-	// 	$('#likecanvas').show();
-	// });
-	/******************* Bind para el boton cerrar del canvas que contiene el boton like */
-	// $('#close').on('tap', function(event){
-	// $('#likecanvas').hide();
-
 	// Acciones asociadas al boton participar.
 	$('#btnjoin').on('click', function(event){
 		// Pedimos los datos y/o los permisos para acceder a los datos.
 		FB.getLoginStatus(function(response){
 			// Verifico si ya tengo los permisos y la aplicacion esta autorizada.
 			if(response && response.authResponse && response.status === 'connected'){
-				console.log(response);
 				$.participantdata = buildParticipantObject(response);
 				// Verificamos si ya participo.
 				if(!isEmpty($.participantdata)){
@@ -46,19 +35,14 @@ $(document).ready(function(){
 			}
 		});
 	});
-
 	// Bindeamos el input y la validacion para capturar el puntaje.
 	$('#btnok').on('click', function(event){
 		var score = parseInt($('#score').val(), 10);
-		console.log(score);
 		if(typeof score === 'number' && (score % 1) === 0 && score > 0){
 			$.participantdata.score = score;
-			console.log($.participantdata);
 			$('#step2').hide();
 			$('#step1').show();
-
-
-
+			// Plugin para hacer el file upload con jquery
 			var uploader = new ss.SimpleUpload({
 				button: 'btntakepic',
 				url: 'src/guardarImagen.php',
@@ -67,9 +51,6 @@ $(document).ready(function(){
 				allowedExtensions: ['jpg', 'jpeg', 'png', 'gif'],
 				maxSize: 1500,
 				onSubmit: function(filename, extension){
-					console.log(filename);
-					console.log(extension);
-					console.log("submit");
 					$('#loader').show();
 				},
 				onComplete: function(filename, response){
@@ -78,9 +59,6 @@ $(document).ready(function(){
 						alert(filename + 'upload failed');
 						return false;
 					}
-					console.log(filename);
-					console.log(response);
-					console.log("uploaded");
 					$.post('src/guardarParticipante.php', { userID: $.participantdata.userID, accessToken: $.participantdata.accessToken, first_name: $.participantdata.first_name, last_name: $.participantdata.last_name, name: $.participantdata.name, email: $.participantdata.email, score: $.participantdata.score },function(data){
 						$('#step1').hide();
 						$('#step3').show();
@@ -88,26 +66,6 @@ $(document).ready(function(){
 					});
 				}
 			});  
-
-
-			// Uploader y post para los datos del concurso.
-			// Bindeamos el file imput para el preload del archivo.
-			// $('#imageform').fileUpload({
-			// 	uploadData    : { userID: $.participantdata.userID }, // Append POST data to the upload
-			// 	submitData    : { userID: $.participantdata.userID, accessToken: $.participantdata.accessToken, first_name: $.participantdata.first_name, last_name: $.participantdata.last_name, name: $.participantdata.name, email: $.participantdata.email, score: $.participantdata.score }, // Append POST data to the form submit
-			// 	uploadOptions : { dataType : 'json' }, // Customise the parameters passed to the $.ajax() call on uploads. You can use any of the normal $.ajax() params
-			// 	submitOptions : { dataType : 'json' }, // Customise the parameters passed to the $.ajax() call on the form submit. You can use any of the normal $.ajax() params
-			// 	before        : function(){}, // Run stuff before the upload happens
-			// 	beforeSubmit  : function(uploadData){ console.log(uploadData); return true; }, // access the data returned by the upload return false to stop the submit ajax call
-			// 	success       : function(data, textStatus, jqXHR){ console.log(data); }, // Callback for the submit success ajax call
-			// 	error         : function(jqXHR, textStatus, errorThrown){ console.log(jqXHR); }, // Callback if an error happens with your upload call or the submit call
-			// 	complete      : function(jqXHR, textStatus){ console.log(jqXHR); } // Callback on completion
-			// });
-			// Acciones asociadas al boton subir foto trigger del action del file uploader.
-			$('#btntakepic').on('click', function(event){
-				$('#imageinput').click();
-				// $('#imageinput').trigger('click');
-			});
 		}else{
 			alert("Es necesario ingresar un puntaje valido.");
 		}
@@ -135,41 +93,9 @@ function verBases(){
 	var WindowObjectReference = window.open("bases.pdf", "bases_concurso", "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes");
 }
 
-/******************* Revisa que este logeado y chequea los permisos asi tambien los solicita de ser necesario */
-function fanGateIni(){
-	/* isFanCheck("211627285577705"); -> Felipe I. González G. */
-	/* isFanCheck("99723704300"); -> Maternidad Clinica Alemana */
-	FB.getLoginStatus(function(response){
-		if(response && response.authResponse && response.status === 'connected'){
-			isFanCheck("256475461058960");
-		}else{
-			FB.login(function(response){
-				if (response && response.authResponse && response.status === 'connected'){
-					isFanCheck("256475461058960");
-				}else{
-					isFanChecknoPermisions();
-				}
-			},{scope:'user_likes, email'});
-		}
-	});
-}
-
-/******************* Verifica que el usuario sea fan de la pagina indicada. */
-function isFanCheck(page_id){
-	FB.api('/me/likes/'+page_id,function(response) {
-		if(response.data){
-			if(!isEmpty(response.data)){
-				$('#step0').show();
-			}else{
-				$('#fangate').show();
-			}
-		}
-	});
-}
-
 /******************* Que despliega mensaje indicando que es necesario los permisos para usar la aplicacion */
 function isFanChecknoPermisions(){
-	alert("Para usar esta aplicación, es necesario concedas los permisos necesarios para corroborar si ya eres fan de esta pagina.");
+	alert("Para usar esta aplicación, es necesario concedas los permisos necesarios.");
 	location.reload();
 }
 
