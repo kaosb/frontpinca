670485b50a840b300f5bd81cc94c7846f2d4e2ab
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-12-27 21:47:46 -0500

    Cleanup vote.php

--- vote.php
+++ vote.php
@@ -6,42 +6,16 @@
 	<title>CPECH</title>
 	<link rel="stylesheet" href="css/style.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>
-	<script src="js/jquery.countdown.min.js" type="text/javascript" charset="utf-8" async defer></script>
 	<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
 </head>
 <body>
-	<div id="welcome">
-		<div id="timmer"></div>
-		<div id="btnverbases"></div>
-	</div>
-	<div id="fangate">
-		<div id="btnlike"></div>
-		<div id="btnverbases"></div>
-	</div>
-	<div id="step0">
-		<div id="btnjoin"></div>
-		<div id="btnverbases"></div>
-	</div>
-	<div id="step1">
-		<div id="btntakepic"></div>
-	</div>
-	<div id="step2">
-		<input id="score" type="text" name="" value="0">
-		<div id="btnok"></div>
-	</div>
-	<div id="step3">
-		<div id="btntwitter"></div>
-		<div id="btnfacebook"></div>
-	</div>
-
-<!-- 	<div id="vote0">
+	<div id="vote0">
 		<div id="imagenpersona">
 			<img src="http://lorempixel.com/300/300/sports/1/">
 		</div>
 		<span id="milagropunto">#milogrocpech fue 900 puntos :)</span>
 		<a href="#" id="btnvotar">Votar</a>
 		<span id="graciasvoto" style="display: none;">¡¡Gracias por tu voto!!</span>
-	</div> -->
-	
+	</div>
 </body>
 </html>
